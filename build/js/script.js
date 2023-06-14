fc926d786be904145301d97331c3876021ce411d
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2023-06-14 21:48:39 +0100

    Prettier Format

--- build/js/script.js
+++ build/js/script.js
@@ -11,27 +11,27 @@
 //     mobileMenu.addEventListener('click',toggleMenu)
 // })()
 
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const mobileMenu = document.getElementById('mobile-menu');
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileMenu = document.getElementById("mobile-menu");
 
 const toggle = () => {
-const isOpened = hamburgerBtn.getAttribute('aria-expanded') === "true";
-    isOpened ? closeMenu() : openMenu();
-}
-
-const openMenu =  ()=>{
-    hamburgerBtn.setAttribute("aria-expanded", true);
-    mobileMenu.setAttribute("aria-expanded", true);
-    hamburgerBtn.classList.add('toggle-btn');
-    mobileMenu.classList.remove('hidden');
+	const isOpened = hamburgerBtn.getAttribute("aria-expanded") === "true";
+	isOpened ? closeMenu() : openMenu();
 };
 
-const closeMenu =  ()=>{
-    hamburgerBtn.setAttribute("aria-expanded", false);
-    mobileMenu.setAttribute("aria-expanded", false);
-    mobileMenu.classList.add('hidden');
-    hamburgerBtn.classList.remove('toggle-btn');
-}
+const openMenu = () => {
+	hamburgerBtn.setAttribute("aria-expanded", true);
+	mobileMenu.setAttribute("aria-expanded", true);
+	hamburgerBtn.classList.add("toggle-btn");
+	mobileMenu.classList.remove("hidden");
+};
 
-hamburgerBtn.addEventListener('click',toggle)
-mobileMenu.addEventListener('click',toggle)
+const closeMenu = () => {
+	hamburgerBtn.setAttribute("aria-expanded", false);
+	mobileMenu.setAttribute("aria-expanded", false);
+	mobileMenu.classList.add("hidden");
+	hamburgerBtn.classList.remove("toggle-btn");
+};
+
+hamburgerBtn.addEventListener("click", toggle);
+mobileMenu.addEventListener("click", toggle);
